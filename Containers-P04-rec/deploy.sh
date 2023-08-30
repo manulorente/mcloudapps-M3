6a1b258eedcd5c8ba254096a9f74f0bdd7350fee
manu <manloralm@outlook.com>
2023-08-30 21:25:53 +0200

    New version

--- Containers-P04-rec/deploy.sh
+++ Containers-P04-rec/deploy.sh
@@ -1,9 +1,23 @@
-helm package ./EoloPlanner -d ./EoloPlanner/charts && helm repo index ./EoloPlanner/charts --url https://raw.githubusercontent.com/manulorente/mcloudapps-M3/main/Containers-P04-rec/EoloPlanner/charts/
+#!/bin/bash
 
+set -e
+
+# Package the Helm chart
+helm package ./EoloPlanner -d ./EoloPlanner/charts
+
+# Create or update the Helm repository index
+helm repo index ./EoloPlanner/charts --url https://raw.githubusercontent.com/manulorente/mcloudapps-M3/main/Containers-P04-rec/EoloPlanner/charts/
+
+# Add the Helm repository
 helm repo add httpd-web-server https://raw.githubusercontent.com/manulorente/mcloudapps-M3/main/Containers-P04-rec/EoloPlanner/charts/
 
-git add . && git commit -m "New version" && git push
+# Commit and push changes to Git
+git add .
+git commit -m "New version"
+git push
 
+# Update Helm repositories
 helm repo update
 
+# Install the Helm chart
 helm install eoloplanner httpd-web-server/EoloPlanner
