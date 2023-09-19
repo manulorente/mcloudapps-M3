1edb1e27eb6052f74db9eefe823191d1c71ca0e9
Manu <manloralm@outlook.com>
2023-09-19 13:28:18 +0200

    Update chaos.sh

--- Containers-P05-rec/webapp-stateless/chaos.sh
+++ Containers-P05-rec/webapp-stateless/chaos.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
+# Randomly delete pods in a Kubernetes namespace.
+set -ex
 
-+ sleep 5
-+ true
-+ kubectl --namespace default -o 'jsonpath={.items[*].metadata.name}' get pods
-+ tr ' ' '\n'
-+ grep -v db
-+ shuf
-+ head -n 1
-+ xargs -t --no-run-if-empty kubectl --namespace default delete pod
+: ${DELAY:=30}
+: ${NAMESPACE:=default}
 
-
+while true; do
+  kubectl \
+    --namespace "${NAMESPACE}" \
+    -o 'jsonpath={.items[*].metadata.name}' \
+    get pods | \
+      tr " " "\n" | \
+      shuf | \
+      head -n 1 |
+      xargs -t --no-run-if-empty \
+        kubectl --namespace "${NAMESPACE}" delete pod
+  sleep "${DELAY}"
+done
